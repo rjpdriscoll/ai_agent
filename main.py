1c48f8de2924ae0b50c7e3394503ceb36a7abf9d
rjpdriscoll <rjpdriscoll@gmail.com>
2025-12-31 20:10:43 -0700

    Chapter 1 of project complete

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -12,11 +13,15 @@ model = "gemini-2.5-flash"
 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
+
+
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 response = client.models.generate_content(
     model=model,
-    contents=args.user_prompt,
+    contents=messages,
 )
 
 
@@ -37,9 +42,11 @@ def get_token_count(token_type):
 def main():
     if api_key == None:
         raise RuntimeError("No Gemini API key was found")
-    print(f"Prompt tokens: {get_token_count('prompt_tokens')}")
-    print(f"Response tokens: {get_token_count('response_tokens')}")
     print(response.text)
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {get_token_count('prompt_tokens')}")
+        print(f"Response tokens: {get_token_count('response_tokens')}")
 
 
 if __name__ == "__main__":
